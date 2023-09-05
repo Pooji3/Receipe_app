7c6c48e83bce79b3510f138964597d9aa1a7a074
Jampala Poojitha <jampalapoojitha2003@gmail.com>
2023-09-05 15:40:26 +0530

    2

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const routes =require("./route/route");
 const recipeController=require("./Controllers/receipeController");
 // const Axios=require("axios")
-var cors = require("cors");
-app.use(cors());
+
 
 const app = express();
 
@@ -25,17 +24,8 @@ mongoose.connect("mongodb+srv://poojitha2003:Jpoojitha2003@cluster0.7zh8jc4.mong
     console.log("Database Connected Successfully");
 
     
-    app.post("/ins", function (req, res) {
-        const fdata = req.body.fdata;
-        conn.collection("backend").insertOne(fdata, (err, result) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("inserted");
-            res.status(202).send("success");
-          }
-        });
-      });
+    
+        
       app.listen(4000, () => {
         console.log("server running at 4000");
       });
